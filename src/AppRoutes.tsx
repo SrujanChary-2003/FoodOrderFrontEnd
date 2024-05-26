0da49a54e3ed9726702f18c3b170056ab977cce2
PADAKANTI SRUJAN CHARY <21r21a3345@mlrinstitutions.ac.in>
2024-05-27 01:42:00 +0530

    added order related settings

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./Auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatus";
 
 const AppRoutes = () =>{
     return (
@@ -18,6 +19,10 @@ const AppRoutes = () =>{
             <Route path="/detail/:restaurantId" element={<Layout showHero={false} ><DetailPage /> </Layout>} />
             
             <Route element={<ProtectedRoute />}>
+            <Route path="/order-status" element={ <Layout>
+                <OrderStatusPage />
+            </Layout>
+            } />
             <Route path="/user-profile" element={ <Layout>
                 <UserProfilePage />
             </Layout>
